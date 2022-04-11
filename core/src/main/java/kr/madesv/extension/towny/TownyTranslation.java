643c75bfeeb2d6f076f1821fa5945759121223d0
Junhyung <vjh0107@naver.com>
2022-04-11 16:03:06 +0900

    minor fixes

--- core/src/main/java/kr/madesv/extension/towny/TownyTranslation.java
+++ core/src/main/java/kr/madesv/extension/towny/TownyTranslation.java
@@ -4,27 +4,31 @@ import com.palmergames.bukkit.towny.object.TownyPermission;
 import lombok.Getter;
 
 public enum TownyTranslation {
-    BUILD(TownyPermission.ActionType.BUILD, "건축"),
-    DESTROY(TownyPermission.ActionType.DESTROY, "파괴"),
-    SWITCH(TownyPermission.ActionType.SWITCH, "스위치"),
-    ITEM_USE(TownyPermission.ActionType.ITEM_USE, "아이템");
+    BUILD(TownyPermission.ActionType.BUILD, "설치", "build"),
+    DESTROY(TownyPermission.ActionType.DESTROY, "파괴", "destroy"),
+    SWITCH(TownyPermission.ActionType.SWITCH, "스위치", "switch"),
+    ITEM_USE(TownyPermission.ActionType.ITEM_USE, "아이템", "itemuse");
+
+    @Getter
+    private final TownyPermission.ActionType actionType;
 
     @Getter
     private final String translatedName;
 
     @Getter
-    private final TownyPermission.ActionType actionType;
+    private final String genuineName;
 
-    TownyTranslation(TownyPermission.ActionType actionType, String translatedName) {
+    TownyTranslation(TownyPermission.ActionType actionType, String translatedName, String genuineName) {
         this.actionType = actionType;
         this.translatedName = translatedName;
+        this.genuineName = genuineName;
     }
 
     public static String untranslateAll(String string) {
         return string
-                .replace(TownyTranslation.BUILD.getTranslatedName(), "build")
-                .replace(TownyTranslation.DESTROY.getTranslatedName(), "destroy")
-                .replace(TownyTranslation.SWITCH.getTranslatedName(), "switch")
-                .replace(TownyTranslation.ITEM_USE.getTranslatedName(), "itemuse");
+                .replace(TownyTranslation.BUILD.getTranslatedName(), TownyTranslation.BUILD.genuineName)
+                .replace(TownyTranslation.DESTROY.getTranslatedName(), TownyTranslation.DESTROY.genuineName)
+                .replace(TownyTranslation.SWITCH.getTranslatedName(), TownyTranslation.SWITCH.genuineName)
+                .replace(TownyTranslation.ITEM_USE.getTranslatedName(), TownyTranslation.ITEM_USE.genuineName);
     }
 }
